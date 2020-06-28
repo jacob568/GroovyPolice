b1d1d97623bc6d06a68040a2cd927ba9c24f7ed4
jacob568 <jacob568@users.noreply.github.com>
2020-06-28 19:52:08 +0100

    Added constants
    Moved response messages to a constants file.
    Only groovy bot commands are allowed in the groovy commands channel

--- GroovyPoliceBot.py
+++ GroovyPoliceBot.py
@@ -2,6 +2,8 @@ import os
 import random
 
 import discord
+import constants
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,76 +14,65 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+	for guild in client.guilds:
+		if guild.name == GUILD:
+			break
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+	print(
+		f'{client.user} is connected to the following guild:\n'
+		f'{guild.name}(id: {guild.id})'
+	)
 
+#{name: strikeInt}
 user_strikes = {}
+
+#Applies a strike to a user who commited an offense
+#Returns true if the third strike is reached
+def ApplyStrike(offender):
+	if offender in user_strikes.keys():
+		user_strikes[offender] += 1
+		if user_strikes[offender] >= 3:
+			user_strikes[offender] = 0
+			return True
+	else:
+		user_strikes[offender] = 1
+		return False
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+	if message.author == client.user:
+		return
 
-    #The commands to search for
-    GroovyBotCommands = [
-        "-play",
-        "-skip",
-        "-seek",
-        "-join",
-        "-next",
-        "-back",
-        "-clear",
-        "-pause",
-        "-resume"
-    ]
-
-    #Responses to offenders
-    TellingOffMessages = [
-    	"PLACE COMMANDS IN THE RIGHT CHANNEL YOU FOOL ",
-    	"are you serious right now ",
-    	"NEE NAW NEE NAW POST IN THE RIGHT CHANNEL ",
-    	"Hey! I'm a groovy police recruit! Please post in the right channel ",
-    	"why don't you listen to me... im just trying to keep the peace ",
-    	"RESPECT MY AUTHORITAH ",
-    	"I'm 2 weeks away from retirement and I have to deal with your s**t ",
-    	"-play police siren"
-    ]
-
-    #command is (hopefully) always the first thing in the message, so need to split and
+    #the groovy bot command is (hopefully) always the first thing in a message, so need to split and
     #access only the first word
-    splitMessage = message.content.split()
-    channel = message.channel.name
+	splitMessage = message.content.split()
+	channel = message.channel.name
+	name = message.author.name
+	printStrikeMessage = False
+	response = ""
+
+	if channel == constants.GROOVYCHANNEL and splitMessage:
+		if splitMessage[0] not in constants.GROOVYBOTCOMMANDS:
+			await message.delete()
+			response = random.choice(constants.TELLINGOFFMESSAGES) + name
+			printStrikeMessage = ApplyStrike(name)
+		if printStrikeMessage:
+			await message.channel.send("3 strikes " + name + "! Know what that means... nothing I haven't programmed a punishment yet")
+		else:
+			await message.channel.send(response)
+
+	if channel != constants.GROOVYCHANNEL and splitMessage:
+		for command in constants.GROOVYBOTCOMMANDS:
+			if splitMessage[0] == command:
+				#Users have three strikes, stored in a dictionary.
+				printStrikeMessage = ApplyStrike(name)
+				response = random.choice(constants.TELLINGOFFMESSAGES) + name
+				await message.delete()
+				#prints the message depending on number of strikes
+				if printStrikeMessage:
+					await message.channel.send("3 strikes " + name + "! Know what that means... nothing I haven't programmed a punishment yet")
+				else:
+					await message.channel.send(response)
 
     
-    if channel != "place-groovy-commands-here" and splitMessage:
-	    name = message.author.name
-	    printStrikeMessage = False
-
-	    for command in GroovyBotCommands:
-	    	if splitMessage[0] == command:
-	    		#Users have three strikes, stored in a dictionary.
-	    		if name in user_strikes.keys():
-	    			user_strikes[name] += 1
-	    			if user_strikes[name] >= 3:
-	    				user_strikes[name] = 0
-	    				printStrikeMessage = True
-	    		#If a first offender, they are added to the dictionary
-	    		else:
-	    			user_strikes[name] = 1
-
-	    		response = random.choice(TellingOffMessages) + name
-    			await message.channel.purge(limit=1)
-
-    			#prints the message depending on number of strikes
-    			if printStrikeMessage:
-	    			await message.channel.send("3 strikes " + name + "! Know what that means... nothing I haven't programmed a punishment yet")
-	    		else:
-    				await message.channel.send(response)
-
 client.run(TOKEN)
---
+++ constants.py
@@ -0,0 +1,24 @@
+GROOVYBOTCOMMANDS = [
+	"-play",
+	"-skip",
+	"-seek",
+	"-join",
+	"-next",
+	"-back",
+	"-clear",
+	"-pause",
+	"-resume"
+]
+
+#Responses to offenders
+TELLINGOFFMESSAGES = [
+	"PLACE COMMANDS IN THE RIGHT CHANNEL YOU FOOL ",
+	"are you serious right now ",
+	"NEE NAW NEE NAW POST IN THE RIGHT CHANNEL ",
+	"Hey! I'm a groovy police recruit! Please post in the right channel ",
+	"why don't you listen to me... im just trying to keep the peace ",
+	"RESPECT MY AUTHORITAH ",
+	"I'm 2 weeks away from retirement and I have to deal with your s**t ",
+]
+
+GROOVYCHANNEL = "place-groovy-commands-here"
